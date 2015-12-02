89009b38148b693a35ba8d15aacaf5e8db2d498e
Ran Tavory <rantav@gmail.com>
2015-12-02 23:30:45 +0200

    Add support for legacy timeline API

--- import.py
+++ import.py
@@ -9,14 +9,25 @@ from py2neo import Node, Relationship
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--drop', help='Drop data first?', action='store_true')
+parser.add_argument('--file', help='File to import')
 parser.add_argument('--forks', help='Handle forks?', action='store_true')
 parser.add_argument('--watches', help='Handle watches (stars)?', action='store_true')
 parser.set_defaults(drop=False)
 args = parser.parse_args()
 
 graph = Graph()
-# graph.schema.create_uniqueness_constraint("Repository", "id")
-# graph.schema.create_uniqueness_constraint("User", "id")
+try:
+  graph.schema.create_uniqueness_constraint("Repository", "full_name")
+except:
+  pass
+try:
+  graph.schema.create_uniqueness_constraint("User", "login")
+except:
+  pass
+try:
+  graph.schema.create_uniqueness_constraint("PullRequest", "id")
+except:
+  pass
 
 if args.drop:
   print '!!! DROPPING DATABASE !!!'
@@ -27,34 +38,53 @@ def load_from_buffer(buffer):
     process_file_contents(f)
 
 def load_from_file(file_name):
-  with gzip.open(file_name, 'rb') as f:
-    process_file_contents(f)
+  if file_name.endswith('.gz'):
+    with gzip.open(file_name, 'rb') as f:
+      process_file_contents(f)
+  else:
+    with open(file_name, 'rb') as f:
+      process_file_contents(f)
 
 def process_file_contents(f):
+  line_number = 0
   for line in f.readlines():
-    event = json.loads(line)
+    line_number += 1
+    try:
+      event = json.loads(line)
+    except Exception as e:
+      print 'Exception processing line %d' % line_number
+      print e
+      continue
     funcs = {
       'RepositoryEvent': create_repository,
       'PullRequestEvent': handle_pull_request,
       'MemberEvent': handle_member,
+      'ForkEvent': handle_fork,
+      'WatchEvent': handle_watch,
     }
-    if args.forks:
-      funcs['ForkEvent'] = handle_fork
-    if args.watches:
-      funcs['WatchEvent'] = handle_watch
+    # Check out optional event hanlers
+    if not args.forks:
+      del funcs['ForkEvent']
+    if not args.watches:
+      del funcs['WatchEvent']
+
     func = funcs.get(event['type'])
     if func:
       func(event)
 
 def add_user(user):
-  node = graph.merge_one('User', 'id', user['id'])
-  node['login'] = user['login']
-  return node
+  if type(user) == unicode:
+    login = user
+  else:
+    login = user['login']
+  return graph.merge_one('User', 'login', login)
+
+def get_user(login):
+  return graph.find_one('User', 'login', login)
 
 def add_repo(repo):
-  node = graph.merge_one('Repository', 'id', repo['id'])
-  node['full_name'] = repo['full_name'] if 'full_name' in repo  else repo['name']
-  return node
+  name = repo['full_name'] if 'full_name' in repo else repo['name']
+  return graph.merge_one('Repository', 'full_name', name)
 
 def log(event):
   print event['type'], event['id'], event['created_at']
@@ -66,12 +96,17 @@ def handle_fork(event):
   # print json.dumps(event, indent=2)
   payload = event['payload']
   log(event)
-  repo = add_repo(event['repo'])
-  forkee = add_repo(payload['forkee'])
-  forkee_owner = add_user(payload['forkee']['owner'])
-  graph.create_unique(Relationship(forkee_owner, "CONTRIBUTED", forkee))
-  graph.create_unique(Relationship(forkee, "FORKED", repo))
-  graph.push(repo, forkee, forkee_owner)
+  if type(payload['forkee']) == int:
+    handle_fork_legacy(event)
+  else:
+    repo = add_repo(event['repo'])
+    forkee = add_repo(payload['forkee'])
+    forkee_owner = add_user(payload['forkee']['owner'])
+    graph.create_unique(Relationship(forkee_owner, "CONTRIBUTED", forkee))
+    graph.create_unique(Relationship(forkee, "FORKED", repo))
+
+def handle_fork_legacy(event):
+  print "handle_fork_legacy not implemented yet"
 
 def handle_watch(event):
   # print json.dumps(event, indent=2)
@@ -81,26 +116,71 @@ def handle_watch(event):
     actor = add_user(event['actor'])
     repo = add_repo(event['repo'])
     graph.create_unique(Relationship(actor, "WATCHES", repo))
-    graph.push(actor, repo)
 
 def handle_member(event):
-  # print json.dumps(event, indent=2)
   payload = event['payload']
   if payload['action'] == 'added':
     log(event)
+    # print json.dumps(event, indent=2)
     member = add_user(payload['member'])
     repo = add_repo(event['repo'])
     actor = add_user(event['actor'])
     graph.create_unique(Relationship(member, "MEMBER", repo))
     graph.create_unique(Relationship(actor, "MEMBER", repo))
-    graph.push(repo, member, actor)
 
 
 def handle_pull_request(event):
   payload = event['payload']
+  legacy_api = type(payload['actor']) == unicode
+  if legacy_api:
+    handle_pull_request_legacy(event)
+  else:
+    actor = event['actor']
+    pull_request = payload['pull_request']
+    if (payload['action'] == 'closed' and pull_request.get('merged') == True):
+      # print json.dumps(event, indent=2)
+      log(event)
+      repo = add_repo(pull_request['base']['repo'])
+
+      contributor = pull_request['user']
+      if contributor is not None:
+        contributor = add_user(contributor)
+        graph.create_unique(Relationship(contributor, "CONTRIBUTED", repo))
+
+      actor = add_user(actor)
+      graph.create_unique(Relationship(actor, "MEMBER", repo))
+
+def handle_pull_request_legacy(event):
+  payload = event['payload']
+  actor = event['actor']
+  pr_id = '%s/pulls/%s' % (event['repo']['name'], payload['number'])
+  if payload['action'] == 'opened':
+    pr = graph.merge_one('PullRequest', 'id', pr_id)
+    puller = add_user(actor)
+    pr.properties['puller'] = actor['login']
+    graph.push(pr)
+  if payload['action'] == 'closed':
+    log(event)
+    repo = add_repo(event['repo'])
+    pr = get_pull_request(pr_id)
+    if pr:
+      # print json.dumps(event, indent=2)
+      contributor = get_user(pr.properties['puller'])
+      graph.delete(pr)
+      if contributor:
+        graph.create_unique(Relationship(contributor, "CONTRIBUTED", repo))
+
+    actor = add_user(actor)
+    graph.create_unique(Relationship(actor, "MEMBER", repo))
+
+def get_pull_request(pr_id):
+  return graph.find_one('PullRequest', 'id', pr_id)
+
+def handle_pull_request_v2(event):
+  print json.dumps(event, indent=2)
+  payload = event['payload']
   pull_request = payload['pull_request']
-  if (payload['action'] == 'closed' and pull_request['merged'] == True):
-    # print json.dumps(event, indent=2)
+  if (payload['action'] == 'closed' and pull_request.get('merged') == True):
     log(event)
     repo = add_repo(pull_request['base']['repo'])
 
@@ -111,11 +191,6 @@ def handle_pull_request(event):
 
     actor = add_user(event['actor'])
     graph.create_unique(Relationship(actor, "MEMBER", repo))
-
-    if contributor is None:
-      graph.push(repo, actor)
-    else:
-      graph.push(repo, actor, contributor)
 
 def generate_hours(from_date, until_date):
   h = from_date
@@ -139,4 +214,7 @@ def load_from_date(date):
     print "=== Loading:  ", h
     download_and_load_hour(h)
 
-load_from_date(datetime.datetime(2015,1,1))
+if args.file:
+  load_from_file(args.file)
+else:
+  load_from_date(datetime.datetime(2015,1,1))
