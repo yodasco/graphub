cf41f2ae5713aa0fc44b53d75ec7ee134a427bb5
Ran Tavory <rantav@gmail.com>
2015-12-15 10:26:44 +0200

    Improve integration tests

--- loader/integration-test.sh
+++ loader/integration-test.sh
@@ -4,6 +4,7 @@ source venv/bin/activate
 
 for f in $(ls test-data)
 do
-  echo "Processing test-data/$f"
-  python import.py --stars --pulls --pushes --members --forks --file  "test-data/$f" --drop
+  cmd="python import.py --stars --pulls --pushes --members --forks --file  "test-data/$f" --log --drop"
+  echo "$cmd"
+  $cmd
 done
